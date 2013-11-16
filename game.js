519b401c702268674413cdd491141ecd9fe3aab7
Jonathan Bowers <jonotron@gmail.com>
2013-11-16 13:32:39 -0800

    Enable highlighting and block creating/erasing

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var createGame = require('voxel-engine');
 var player = require('voxel-player');
+var highlight = require('voxel-highlight');
 
 var game = createGame({
   generateChunks: false,
@@ -27,6 +28,35 @@ var createPlayer = player(game);
 var avatar = createPlayer('player.png');
 avatar.yaw.position.set(0, 10, 0);
 avatar.possess();
+
+// enable highlighting for block placement and removal
+var blockPosPlace;
+var blockPosErase;
+
+var hl = game.highlighter = highlight(game, {color: 0x0000cc });
+hl.on('highlight',          function (voxelPos) { blockPosErase = voxelPos });
+hl.on('remove',             function (voxelPos) { blockPosErase = null });
+hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos });
+hl.on('remove-adjacent',    function (voxelPos) { blockPosPlace = null });
+
+var currentMaterial = 1;
+var firing = false;
+
+game.on('fire', function (target, state) {
+  if (firing) return;
+  
+  firing = true;
+  setTimeout(function() { firing = false; }, 100);
+
+  var position = blockPosPlace;
+  if (position) {
+    game.createBlock(position, currentMaterial) 
+  } else {
+    position = blockPosErase;
+    if (position) game.setBlock(position, 0);
+  }
+});
+
 
 // attach the game to the body of the webpage
 var container = document.body;
